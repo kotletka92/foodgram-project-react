1afd8f21c3b7c616ea3e7cf5a9e21a06d45c1b28
Антон Филатов <filatovap2013@yandex.ru>
2023-01-30 23:37:22 +0300

    first project1

--- backend/hodgepodge/api/filters.py
+++ backend/hodgepodge/api/filters.py
@@ -12,7 +12,7 @@ class IngredientFilter(SearchFilter):
         fields = ('name', )
 
 
-class RecipeFilter(FilterSet):
+class RecipeFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
